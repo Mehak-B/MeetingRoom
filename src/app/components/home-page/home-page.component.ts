b7385b134d2541118841884e3481a2d3d7c3fc25
Mehak-B <mehakbhardwaj2013@gmail.com>
2025-01-11 19:46:18 +0530

    ad

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
+  loginForm!:FormGroup;
+  // constructor(private formBuilder : FormBuilder) { }
+
+
+ngOnInit(): void {
+  
+  this.loginForm = new FormGroup({
+    email: new FormControl("",[Validators.required, Validators.email]),
+    password: new FormControl("",[Validators.required])
+  })
+}
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      console.log(this._v());
+    }
+  }
+  _v() {
+    return this.loginForm.value;
+  }
 }
